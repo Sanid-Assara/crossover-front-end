94d9cba8f81d8f122f1fa313426afbe9001bb3f0
unknown <sebastian-olthoff@gmx.de>
2024-08-12 14:08:14 +0200

    added edit function and some bugfixing

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,23 @@
-
 import { useState } from "react";
 import AddTodoPopup from "./AddTodoPopup";
+import EditTodoPopup from "./EditTodoPopup";
 
-function TodoList({ todos }) {
+function TodoList({ todos, onEdit }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const [currentTodo, setCurrentTodo] = useState(null);
 
   const handleCreateTask = (newTask) => {
     setTasks([...tasks, newTask]);
   };
-  return (
 
+  const handleEditTask = (updatedTodo) => {
+    onEdit(updatedTodo);
+    setIsEditModalOpen(false);
+  };
+
+  return (
     <div>
       <button onClick={() => setIsModalOpen(true)} className="btn btn-primary">
         Add New Task
@@ -20,18 +27,33 @@ function TodoList({ todos }) {
         onClose={() => setIsModalOpen(false)}
         onCreate={handleCreateTask}
       />
+      {currentTodo && (
+        <EditTodoPopup
+          isOpen={isEditModalOpen}
+          onClose={() => setIsEditModalOpen(false)}
+          onEdit={handleEditTask}
+          todo={currentTodo}
+        />
+      )}
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
             <h2>{todo.title}</h2>
             <p>{todo.description}</p>
             <p>{todo.date}</p>
+            <button
+              onClick={() => {
+                setCurrentTodo(todo);
+                setIsEditModalOpen(true);
+              }}
+              className="btn btn-secondary"
+            >
+              Edit todo
+            </button>
           </li>
         ))}
       </ul>
     </div>
-
-   
   );
 }
 
